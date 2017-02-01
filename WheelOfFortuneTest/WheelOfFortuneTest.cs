7416a7817f2fb72490c4258496ed94432086efee
gusromero <gusromero@gmail.com>
2017-02-01 22:05:51 +0100

    First test in green

---
+++ WheelOfFortuneTest/WheelOfFortuneTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WheelOfFortuneTest
+{
+
+    [TestFixture]
+    public class WheelOfFortuneTest
+    {
+        [Test]
+        public void AddTwoPlusTwo()
+        {
+            Assert.AreEqual(4, 2 + 2);
+        }
+    }
+}
