1fc25be780467e4aa1a8d510f65c6c1ce022ff12
gusromero <gusromero@users.noreply.github.com>
2017-08-11 12:28:41 +0200

    Add Word

---
+++ WheelOfFortuneHelper/Word.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WheelOfFortuneHelper
+{
+    public class Word
+    {
+        private List<Letter> _letters;
+
+        public Word()
+        :this(new List<Letter>())
+        {
+            
+        }
+
+        public Word(List<Letter> letters)
+        {
+            _letters = letters;
+        }
+
+        public string ToRegExp()
+        {
+            return _letters.Aggregate(string.Empty, (current, letter) => current + letter.ToRegExp());
+        }
+    }
+}
