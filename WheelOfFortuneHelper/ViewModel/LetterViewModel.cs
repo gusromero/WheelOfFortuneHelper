495c9d7f182a7897eb0512ab0a999fadde180563
gusromero <gusromero@users.noreply.github.com>
2017-08-14 13:01:34 +0200

    Add View and ViewModel

---
+++ WheelOfFortuneHelper/ViewModel/LetterViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WheelOfFortuneHelper.Model;
+
+namespace WheelOfFortuneHelper.ViewModel
+{
+    public class LetterViewModel
+    {
+        private Letter _letter;
+
+    }
+}
---
+++ WheelOfFortuneHelper/ViewModel/WordsFinderViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WheelOfFortuneHelper.ViewModel
+{
+    public class WordsFinderViewModel
+    {
+        private WordsReader _wordsReader;
+        private WordsFinder _wordsFinder;
+
+        private List<LetterViewModel> _letters;
+
+        public WordsFinderViewModel()
+        {
+            _wordsReader = new WordsReader();
+            var wordList = _wordsReader.GetWordList();
+            _wordsFinder = new WordsFinder(wordList);
+        }
+
+
+    }
+
+  
+}
