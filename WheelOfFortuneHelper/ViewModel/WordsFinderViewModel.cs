43da8c1eb5ae99f894b50ae4cefa137009ca31a5
gusromero <gusromero@users.noreply.github.com>
2017-08-14 14:56:20 +0200

    Add ViewModelBase

--- WheelOfFortuneHelper/ViewModel/WordsFinderViewModel.cs
+++ WheelOfFortuneHelper/ViewModel/WordsFinderViewModel.cs
@@ -1,17 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using WheelOfFortuneHelper.Model;
 
 namespace WheelOfFortuneHelper.ViewModel
 {
-    public class WordsFinderViewModel
+    public class WordsFinderViewModel : ViewModelBase
     {
         private WordsReader _wordsReader;
         private WordsFinder _wordsFinder;
 
-        private List<LetterViewModel> _letters;
+        private int _numberOfLetters;
+        public int NumberOfLetters
+        {
+            get { return _numberOfLetters; }
+            set
+            {
+                _numberOfLetters = value;
+                Letters = new ObservableCollection<Letter>(new Letter[_numberOfLetters]);
+            }
+        }
+
+        private ObservableCollection<Letter> _letters;
+
+        public ObservableCollection<Letter> Letters
+        {
+            get { return _letters; }
+            set
+            {
+                if (value != _letters)
+                {
+                    _letters = value;
+                    OnPropertyChanged("Letters");
+                }
+            }
+        }
+
 
         public WordsFinderViewModel()
         {
